0199316783ceb98cd81ad16276495598b0a2494a
nsanzimfura-eric <erickykress1@gmail.com>
2024-02-09 09:21:11 +0200

    Login and sign up design

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,11 +1,72 @@
 import Footer from "../../ui/Footer/Footer";
+import { apiRoutes } from "../../utils/constants";
 import styles from "./login.module.scss";
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBInput
+}
+    from 'mdb-react-ui-kit';
 
 const Login = () => {
 
     return (
         <div className={styles.login}>
-            login
+            <MDBContainer className="my-5 gradient-form">
+
+                <MDBRow>
+
+                    <MDBCol col='6' className="mb-5">
+                        <div className="d-flex flex-column ms-5">
+
+                            <div className="text-center">
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                                    style={{ width: '185px' }} alt="logo" />
+                                <h4 className="mt-1 mb-5 pb-1">We are The Book Store Team</h4>
+                            </div>
+
+                            <p>Please login to your account</p>
+
+
+                            <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' required />
+                            <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' required />
+
+
+                            <div className="text-center pt-1 mb-5 pb-1">
+                                <button className="mb-4 w-100 btn btn-primary gradient-custom-2">Sign in</button>
+                                <a className="text-muted" href="#!">Forgot password?</a>
+                            </div>
+
+                            <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
+                                <p className="mb-0">Don&apos;t have an account?</p>
+                                <a href={apiRoutes.register}>
+                                    <button className='mx-2 btn btn-secondary' color='primary'>
+                                        Register
+                                    </button>
+                                </a>
+                            </div>
+
+                        </div>
+
+                    </MDBCol>
+
+                    <MDBCol col='6' className="mb-5">
+                        <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
+
+                            <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+                                <h4 className="mb-4">We are more than just a company</h4>
+                                <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
             <Footer />
         </div>
     )
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,11 +1,33 @@
 import Footer from "../../ui/Footer/Footer";
+import { apiRoutes } from "../../utils/constants";
 import styles from "./register.module.scss";
+import { MDBContainer, MDBCol, MDBRow, MDBInput } from 'mdb-react-ui-kit';
 
 const Register = () => {
 
     return (
         <div className={styles.register}>
-            register
+            <MDBContainer fluid className="p-3 my-5 h-custom">
+
+                <MDBRow>
+
+                    <MDBCol col='10' md='6'>
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" className="img-fluid" alt="Sample image" />
+                    </MDBCol>
+
+                    <MDBCol col='4' md='6'>
+                        <MDBInput wrapperClass='mb-4' label='Full Name' id='formControlLg' type='text' size="lg" />
+                        <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" />
+                        <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" />
+                        <div className='text-center text-md-start mt-4 pt-2'>
+                            <button className="mb-0 px-5 w-100 btn btn-primary">Register</button>
+                            <p className="small fw-bold mt-2 pt-1 mb-2">Have an account? <a href={apiRoutes.login} className="link-danger">Login</a></p>
+                        </div>
+
+                    </MDBCol>
+
+                </MDBRow>
+            </MDBContainer>
             <Footer />
         </div>
     )
